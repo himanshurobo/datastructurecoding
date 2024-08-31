9f404ed7e2280b9757b07fcf951e67072e6fca3c
Himanshu Srivastava <14348226+himanshurobo@users.noreply.github.com>
2024-08-31 23:45:59 +0530

    Update MinCostClimbingStairs_746.cpp

--- array/MinCostClimbingStairs_746.cpp
+++ array/MinCostClimbingStairs_746.cpp
@@ -27,7 +27,7 @@ TimeComplexity : O(n)
 
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
+    int minCostClimbingStairs1(vector<int>& cost) {
         
         vector<int>dp(cost.size());
         
@@ -40,6 +40,23 @@ public:
         
         return min(dp[cost.size()-1],dp[cost.size()-2]);
         
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        int n = cost.size();
+        vector<int> minCost(n);
+        minCost[0] = cost[0];
+        minCost[1] = cost[1];
+
+        
+
+        for ( int i = 2;  i < cost.size(); i++){
+            minCost[i] = cost[i] + min(minCost[i-1],minCost[i-2]);
+
+        }
+
+        return min(minCost[n-1],minCost[n-2]);
     }
 };
 
