1349f0fddf92ac9b365d622dd98fe14d59681e84
Himanshu Srivastava <14348226+himanshurobo@users.noreply.github.com>
2024-09-01 11:34:10 +0530

    Update NumberOfSubarraysWithBoundedMaximum_795.cpp

--- array/NumberOfSubarraysWithBoundedMaximum_795.cpp
+++ array/NumberOfSubarraysWithBoundedMaximum_795.cpp
@@ -27,7 +27,7 @@ TimeComplexity : O(n)
 
 class Solution {
 public:
-    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+    int numSubarrayBoundedMax1(vector<int>& nums, int left, int right) {
         int result=0;
         int l =-1;
         int r =-1;
@@ -38,5 +38,30 @@ public:
             // cout<<result<<" "<<l<<" "<<r<<endl;
         }
         return result;
+    }
+
+int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+
+        int i =0;
+        int j = 0;
+        int maxCount = 0;
+        int m = 0 ;
+
+        while( j < nums.size()){
+
+            if ( nums[j]> right ){
+                m = 0;
+                i = j +1;
+                
+            }else if( nums[j]>= left && nums[j] <= right){
+                m = j - i +1;
+            }
+
+            maxCount += m;
+            j++;
+        }
+
+        return maxCount;
+        
     }
 };
