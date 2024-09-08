905577a7ac183ebe213cd71c9a8cae64553b1f71
Himanshu Srivastava <14348226+himanshurobo@users.noreply.github.com>
2024-09-08 19:16:38 +0530

    Update FindLargestValueInEachTreeRow_515.cpp

--- tree/FindLargestValueInEachTreeRow_515.cpp
+++ tree/FindLargestValueInEachTreeRow_515.cpp
@@ -38,7 +38,7 @@ class Solution {
     
     vector<int> result;
     
-    void largetValue(TreeNode* root, int level){
+    void largetValue1(TreeNode* root, int level){
         if(root == NULL){ 
              return;
         }
@@ -48,16 +48,49 @@ class Solution {
             
         result[level-1] =  std::max( result[level-1],root->val);
         
-        largetValue(root->left,level+1);
-        largetValue(root->right,level+1);
+        largetValue1(root->left,level+1);
+        largetValue1(root->right,level+1);
   
         
     }
     
 public:
-    vector<int> largestValues(TreeNode* root) {
+    vector<int> largestValues1(TreeNode* root) {
 
-        largetValue(root,1);
+        largetValue1(root,1);
+        return result;
+    }
+
+ vector<int> largestValues(TreeNode* root) {
+        vector<int> result;
+        if (root == nullptr) {
+            return result; // Return empty if the tree is empty
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int levelSize = q.size();  // Number of nodes at the current level
+            int maxVal = INT_MIN;     // Initialize to the smallest possible integer
+            
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                
+                maxVal = max(maxVal, node->val);  // Update maxVal
+                
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
+            }
+            
+            result.push_back(maxVal);  // Add the largest value of the current level to the result
+        }
+        
         return result;
     }
 };
