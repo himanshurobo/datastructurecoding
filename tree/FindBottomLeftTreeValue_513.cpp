afa952c5959ae5afdc0fd44219c246845be52bb1
Himanshu Srivastava <14348226+himanshurobo@users.noreply.github.com>
2024-09-08 19:19:06 +0530

    Update FindBottomLeftTreeValue_513.cpp

--- tree/FindBottomLeftTreeValue_513.cpp
+++ tree/FindBottomLeftTreeValue_513.cpp
@@ -46,7 +46,7 @@ class Solution {
     int depth = 0;
     int leftMost = 0;
     
-    void findBottomLeftVlue(TreeNode* root, int level){
+    void findBottomLeftVlue1(TreeNode* root, int level){
         
         if(depth < level){            
             leftMost = root->val;
@@ -54,18 +54,55 @@ class Solution {
         }
         
         if(root->left != NULL){
-            findBottomLeftVlue(root->left,level+1);
+            findBottomLeftVlue1(root->left,level+1);
         }
         
          if(root->right != NULL){
-            findBottomLeftVlue(root->right,level+1);
+            findBottomLeftVlue1(root->right,level+1);
         }
             
     }
     
 public:
-    int findBottomLeftValue(TreeNode* root) {
-        findBottomLeftVlue(root,1);
+    int findBottomLeftValue1(TreeNode* root) {
+        findBottomLeftVlue1(root,1);
         return leftMost;
     }
+
+int findBottomLeftValue(TreeNode* root) {
+        if (root == NULL) {
+            return -1; // Return a default value if the tree is empty
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        int bottomLeftValue = root->val; // Initialize with the root value
+        
+        while (!q.empty()) {
+            int levelSize = q.size();
+            
+            // Traverse the current level
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                
+                // The first node in the current level is the leftmost node
+                if (i == 0) {
+                    bottomLeftValue = node->val;
+                }
+                
+                if (node->left != NULL) {
+                    q.push(node->left);
+                }
+                if (node->right != NULL) {
+                    q.push(node->right);
+                }
+            }
+        }
+        
+        return bottomLeftValue;
+    }
+
+
 };
