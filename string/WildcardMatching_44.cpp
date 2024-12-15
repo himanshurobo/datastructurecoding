3d52b1c883c613aa0bdd0c8519227fb536bdaa28
Himanshu Srivastava <14348226+himanshurobo@users.noreply.github.com>
2024-12-15 19:43:22 +0530

    Update WildcardMatching_44.cpp

--- string/WildcardMatching_44.cpp
+++ string/WildcardMatching_44.cpp
@@ -48,50 +48,98 @@ Output: false
 WrittenBy : Himanshu Srivastava
 Problem : Wildcard Matching
 TimeComplexity : O(n)
-
-
 */
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int m = s.length(), n = p.length();
-        int i = 0, j = 0, asterisk = -1, match;
-        while (i < m) {
-            if (j < n && p[j] == '*') {
-                match = i; 
-                asterisk = j++;
-            }
-            else if (j < n && (s[i] == p[j] || p[j] == '?')) {
-                i++; 
-                j++;
-            }
-            else if (asterisk >= 0) {
-                i = ++match;
-                j = asterisk + 1;
-            }
-            else return false;
+        int m = s.size(), n = p.size();
+        
+        // DP table where dp[i][j] means if s[0..i-1] matches p[0..j-1]
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        
+        // Base cases
+        dp[0][0] = true;  // Empty string matches empty pattern
+        
+        // Handle patterns like "*", "**", etc., that match an empty string
+        for (int j = 1; j <= n; j++) {
+            if (p[j - 1] == '*')
+                dp[0][j] = dp[0][j - 1];
         }
-        while (j < n && p[j] == '*') j++;
-        return j == n;
+        
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+        
+        return dp[m][n];
     }
 };
 
-
-void displayInt(vector<string>input){
-    cout<<"Displaying size "<<input.size()<<endl;
-    for (std::vector<string>::size_type i = 0; i < input.size(); i++) {
-        std::cout << input.at(i) << ' ';
-    }
-}
-
-int main(){
-
-    Solution sol;
-
-    vector<string>input{"abcabcbb"}
-
-    displayInt(sol.isMatch(input));
+int main() {
+    Solution solution;
+    string s = "adceb";
+    string p = "*a*b";
+    cout << (solution.isMatch(s, p) ? "True" : "False") << endl;
 
     return 0;
 }
+
+------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// class Solution {
+// public:
+//     bool isMatch(string s, string p) {
+//         int m = s.length(), n = p.length();
+//         int i = 0, j = 0, asterisk = -1, match;
+//         while (i < m) {
+//             if (j < n && p[j] == '*') {
+//                 match = i; 
+//                 asterisk = j++;
+//             }
+//             else if (j < n && (s[i] == p[j] || p[j] == '?')) {
+//                 i++; 
+//                 j++;
+//             }
+//             else if (asterisk >= 0) {
+//                 i = ++match;
+//                 j = asterisk + 1;
+//             }
+//             else return false;
+//         }
+//         while (j < n && p[j] == '*') j++;
+//         return j == n;
+//     }
+// };
+
+
+// void displayInt(vector<string>input){
+//     cout<<"Displaying size "<<input.size()<<endl;
+//     for (std::vector<string>::size_type i = 0; i < input.size(); i++) {
+//         std::cout << input.at(i) << ' ';
+//     }
+// }
+
+// int main(){
+
+//     Solution sol;
+
+//     vector<string>input{"abcabcbb"}
+
+//     displayInt(sol.isMatch(input));
+
+//     return 0;
+// }
